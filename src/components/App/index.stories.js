42f4b8830d01e847a4c1bd803d400734f80ab9c4
Adrien Vidal <adriensg.vidal@gmail.com>
2020-10-05 17:47:17 +0200

    add storybook

---
+++ src/components/App/index.stories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import App from '.'
+import '../../assets/styles/global.scss'
+
+export default {
+	component: App,
+	title: 'Components/App',
+}
+
+export const Default = () => (
+	<App />
+
+)
